3b4a2630b2a0dc2a7ca057e3be8e21e7d5884cf1
maducalado <calado.maria@escola.pr.gov.br>
2026-04-06 08:38:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+ const botoes = document.querySelecconsttorAll(".botao");
+
+ for(let i = 0; i < botoes.length; i++){
+     botoes[i].onclickc = function(){
+        botoes[i].classlist.add("ativo");
+     }
+ }
